57ea76ce4503e940fbbf4e045a22869ee7fff8f0
unknown <evallardy@gmail.com>
2024-12-19 13:04:23 -0600

    1a Version 17

---
+++ capacitacion/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+from core.views import index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('core/', include('core.urls')),
+    path('usuario/', include('usuario.urls')),
+    path('feriados/', include('feriados.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
